401ec99e7355218836e539fa507438eb13774af1
nikhil <nkmittal4994@gmail.com>
2015-06-17 22:11:20 +0530

    Fetching more data...all scrobbles

--- scrobble.py
+++ scrobble.py
@@ -7,11 +7,25 @@ my_api_key = "829b12779f358d0af117bef698aaa7e2"
 url = "http://ws.audioscrobbler.com/2.0/?"
 
 params = urllib.parse.urlencode(
-    {'method': 'user.getRecentTracks', 'user': 'nickedes', 'format': 'json',
+    {'method': 'user.getRecentTracks', 'user': 'nickedes', 'format': 'json', 'page': 1,
      'api_key': my_api_key})
 
 site = urllib.request.urlopen(url+params)
 
 str_response = site.readall().decode('utf-8')
 obj = json.loads(str_response)
-print(obj['recenttracks']['@attr'][ 'totalPages'])
+print(obj)
+totalPages = obj['recenttracks']['@attr']['totalPages']
+
+all_data = {}
+all_data[1] = obj
+for page in range(2, totalPages):
+    params = urllib.parse.urlencode(
+        {'method': 'user.getRecentTracks', 'user': 'nickedes', 'format': 'json', 'page': page,
+         'api_key': my_api_key})
+    site = urllib.request.urlopen(url+params)
+    str_response = site.readall().decode('utf-8')
+    all_data[page] = json.loads(str_response)
+
+# Todo: write all date to different files. "1.json" <-something lyk dis.
+print(all_data)
