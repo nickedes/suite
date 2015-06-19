4e52412bca9c28348fdb34aa297a67b85a4874d8
nikhil <nkmittal4994@gmail.com>
2015-06-19 23:17:45 +0530

    a plot of albums and it's count

--- R/try.R
+++ R/try.R
@@ -1,5 +1,8 @@
 # Trying some plots! Coz sometimes u don't know what u want 
-dat = read.csv(file="/home/nickedes/Workeda/suite/data/nickedes.csv",header=TRUE,sep=",")
+dat = read.csv(file="/home/nickedes/Workeda/suite/data/albums.csv",header=TRUE,sep=",")
 library(ggplot2)
 # Density plot
-ggplot(dat, aes(x=Artist)) + geom_density()
+g<-ggplot(data=dat, aes(x=Album,y=count)) + geom_bar(stat="identity")
+g<-g+ggtitle('Albums')
+g<-g + theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5))
+g
--- conv.py
+++ conv.py
@@ -5,19 +5,29 @@ import csv
 with open('data/nickedes.csv', newline='') as csvfile:
     data = csv.reader(csvfile, delimiter=',')
     artists = {}
+    albums = {}
     for row in data:
         if row[0] not in artists:
             artists[row[0]] = 1
         else:
             artists[row[0]] += 1
-    avg = 0
-    for data in artists:
-        avg += artists[data]
-    avg = avg//len(artists)
-    print(avg)
+        if row[1] not in albums:
+            albums[row[1]] = 1
+        else:
+            albums[row[1]] += 1
+    avg_artist = sum(artists.values())//len(artists)
+    avg_album = sum(albums.values())//len(albums)
+    print(albums)
 with open('data/artists.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(["Artist", "count", "average"])
     for data in artists:
-        if artists[data] >= avg:
-            writer.writerow([data, artists[data], avg])
+        if artists[data] >= avg_artist:
+            writer.writerow([data, artists[data]])
+
+with open('data/albums.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(["Album", "count"])
+    for data in albums:
+        if albums[data] >= avg_album:
+            writer.writerow([data, albums[data]])
