d0328f4367c220101f86729509e15f55f0718bfc
nikhil <nkmittal4994@gmail.com>
2015-06-19 21:33:28 +0530

    PLaying with time of scrobbles!!

--- time.py
+++ time.py
@@ -1,8 +1,19 @@
 # Convert data of scrobbles to tell about no. of plays during a time period.
 import csv
 
+time_dict = {}
 with open('data/nickedes.csv', newline='') as csvfile:
     data = csv.reader(csvfile, delimiter=',')
+    i = 0
     for row in data:
-        # Todo: process datetime!
-        print(row[2])
+        i += 1
+        if i != 1:
+            try:
+                ranges = row[3][11:row[3].index(':')] + "-" + str(int(row[3][11:row[3].index(':')])+1)
+                if ranges not in time_dict:
+                    time_dict[ranges]=1
+                else:
+                    time_dict[ranges]+=1
+            except:
+                pass
+print(sum(time_dict.values()))
